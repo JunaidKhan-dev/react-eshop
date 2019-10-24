18ef6535149d86fc0972c49d22f3b2f9a2d94ea6
junaidkhanEU <junaidkhan1@gmx.com>
2019-10-24 14:57:21 +0100

    sass added with simple structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
-
+import HomePage from './Homepage.component'
 function App () {
   return (
     <div>
-     E-Shop
+      <HomePage />
     </div>
   )
 }
