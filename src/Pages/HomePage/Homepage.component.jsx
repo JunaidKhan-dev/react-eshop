21ae9a559d0900c8db76b991021ba26e9f6e56c4
junaidkhanEU <junaidkhan1@gmx.com>
2019-10-25 09:48:39 +0100

    hompage with smaller component with style added

---
+++ src/Pages/HomePage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../Components/Directory/Directory.component'
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  )
+}
+
+export default HomePage
