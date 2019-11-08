39aa49f376d871656879ce23c3d3a49e2a0f3466
junaidkhanEU <junaidkhan1@gmx.com>
2019-11-08 13:38:27 +0000

    fix

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,7 +2,7 @@ import { createSelector } from 'reselect'
 
 // first you need input selector to create a memoized part of selector state, usually this would be on single level from main state
 const selectCart = state => state.cart
-
+// create a memoized selector for react state we need to use createSelection function which take two argument , a piece of state and function which take that piece of state and return other piece of state base on input piece of state
 const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems
